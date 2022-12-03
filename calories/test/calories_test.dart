64215a4f2e1f65d5a4a94660aaeaf338d7ccc97b
Patrick Martin <pux1987@gmail.com>
2022-12-02 21:45:48 -0700

    Solved part 2 of day 1

--- calories/test/calories_test.dart
+++ calories/test/calories_test.dart
@@ -17,4 +17,31 @@ void main() {
   test('Handles winner first', () {
     expect(parseCalories(['1', '2', '3', '', '4']), 6);
   });
+
+  test('Handles three elves', () {
+    expect(parseCalories(['1', '', '2', '', '3'], elfCount: 3), 6);
+  });
+
+  test('Handles duplicates', () {
+    expect(parseCalories(['1', '', '1', '', '1'], elfCount: 3), 3);
+  });
+
+  final largeSample = '''1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000''';
+
+  test('Finds the top 3', () {
+    expect(parseCalories(largeSample.split('\n'), elfCount: 3), 45000);
+  });
 }
